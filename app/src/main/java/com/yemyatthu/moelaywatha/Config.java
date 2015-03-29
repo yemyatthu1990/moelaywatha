c0f88d8bc66aa6945bb20c49bd295f9213201d19
Ye Myat Thu <thu.yemyat@gmail.com>
2015-03-29 18:10:01 +0630

    Add Config.java to git ignore

--- app/src/main/java/com/yemyatthu/moelaywatha/Config.java
+++ app/src/main/java/com/yemyatthu/moelaywatha/Config.java
@@ -8,5 +8,5 @@ public class Config {
   //Preferences TAG
   public static final String LAST_LATITUDE = "last_latitude";
   public static final String LAST_LONGITUDE = "last_longitude";
-  public static final String API_KEY = "******";
+  public static final String API_KEY = "4c0c881b8b748f992cb8994a6c18fc3c";
 }
